31b3f02a7bda7a536397b0581a934fbddee90380
MINGI JU <jmgman@outlook.kr>
2023-01-09 22:11:44 +0900

    시간 초과 오류 해결 증진하세요 이새진 학생.

--- 복갱프/이새진_단어중복_1157.cpp
+++ 복갱프/이새진_단어중복_1157.cpp
@@ -9,10 +9,16 @@ int main() {
 	char sub_lan;
 	int a = 0;
 	int end = 0;
-	char alpha = 'a';
+	
 
-	scanf("%s", &lan);
+	scanf("%s", lan);
 	max = strlen(lan);
+
+	/* 
+		일일히 모든 문자를 대문자로 바꿀 필요가 없다. 
+		정답이 나오면 해당하는 문자만 대문자로 바꿔 출력하면 
+		시간초과오류 해결가능
+	*/
 
 	for (int j = 0; j <= max; j++) {
 		if ((lan[j] >= 'a') && (lan[j] <= 'z'))
@@ -42,13 +48,7 @@ int main() {
 			printf("%c", sub_lan);
 			break;
 		}
-		if (alpha == lan[a]) {
-			end += 1;
-			alpha++;
-		}
-		if (end == 26) {
-			break;
-		}
+		
 		a++;
 	}
 
